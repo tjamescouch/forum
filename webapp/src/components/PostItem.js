ad2196501a3b7cee99dff836f0530ded7570b857
James Couch <timothy.james.couch@gmail.com>
2021-08-14 21:13:28 -0600

    -add user name

--- webapp/src/components/PostItem.js
+++ webapp/src/components/PostItem.js
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 function PostItem({post}) {
   return (
     <Link className="Posts-forum-post" to={`/post/${post._id}`}>
-      <img src='/default-avatar.jpeg' />
+      <div className="Posts-avatar-wrapper">
+        <img src='/default-avatar.jpeg' />
+        <p className="Posts-avatar-name" >{post.author && post.author.name}</p>
+      </div>
 
       <div>
         <h3>{post.title}</h3>
